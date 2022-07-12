02eec593684aeb05ec59431c11c4a3f77e9c0857
Maliky853 <maliky853@gmail.com>
2022-07-13 04:19:01 +0530

    completed

--- lucky.js
+++ lucky.js
@@ -5,20 +5,25 @@ let luckyNumber = document.querySelector("#lucky-number")
 let checkNumber = document.querySelector("#check-number")
 let outputBox = document.querySelector("#output-div")
 
-function compareValue(sum,luckyNumber){
-if(sum%luckyNumber===0){
+
+function compareValue(sum,luck){
+    console.log(sum)
+    console.log(luck)
+if(sum%luck===0){
     outputBox.innerText = "your birthday is lucky"
-}else{
+}else {
     outputBox.innerText = "your birthday is not lucky"
 }
 
 }
 
 function checkBirthDateIsLucky(){
-    let dob = dateOfBirth.value
-    let sum = calculateSum(dob)
-    compareValue(sum,luckyNumber)
-
+    let luck = Number(luckyNumber.value)
+    
+        let dob = dateOfBirth.value
+        let sum = calculateSum(dob)
+        compareValue(sum,luck)
+    
 }
 
 function calculateSum(dob){
@@ -30,4 +35,4 @@ function calculateSum(dob){
     return sum
 }
 
-checkNumber.addEventListener("click", checkBirthDateIsLucky )
+checkNumber.addEventListener("click", checkBirthDateIsLucky)
